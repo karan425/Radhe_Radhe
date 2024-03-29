52225dee3cc37f8adca49f27f87ed7e184a6e56e
Karan Kumar Gorai <84957676+karan425@users.noreply.github.com>
2024-03-29 16:39:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,3 +12,31 @@ function downloadImage() {
      link.click();
      document.body.removeChild(link);
 }
+function downloadCard() {
+    // URL of the image you want to download
+    var imageUrl = "./card.jpg";
+
+     // Create a new anchor element
+     var link = document.createElement('a');
+     link.href = imageUrl;
+     link.download = 'image'; // Set the default file name
+     
+     // Trigger the click event on the anchor element
+     document.body.appendChild(link);
+     link.click();
+     document.body.removeChild(link);
+}
+function downloadPamplate() {
+    // URL of the image you want to download
+    var imageUrl = "./pamplate.jpg";
+
+     // Create a new anchor element
+     var link = document.createElement('a');
+     link.href = imageUrl;
+     link.download = 'image'; // Set the default file name
+     
+     // Trigger the click event on the anchor element
+     document.body.appendChild(link);
+     link.click();
+     document.body.removeChild(link);
+}
